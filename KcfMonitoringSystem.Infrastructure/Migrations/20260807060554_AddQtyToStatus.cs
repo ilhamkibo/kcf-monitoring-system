66b6073cb2d4461ea06814a68b708342b2d88679
ilhamkibo <ilham.ilam59@gmail.com>
2026-08-07 13:07:40 +0700

    feat: add Qty property to Status entity and apply database migration

---
+++ KcfMonitoringSystem.Infrastructure/Migrations/20260807060554_AddQtyToStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KcfMonitoringSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQtyToStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Qty",
+                table: "Statuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Qty",
+                table: "Statuses");
+        }
+    }
+}
